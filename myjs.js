786d137cf235b43e5f3ee93957a14ddfc499e9a1
Eshan Shah <eshanshah7@gmail.com>
2017-07-25 14:19:43 -0700

    Added torso. Fixed leg pivot point to top of leg.

--- myjs.js
+++ myjs.js
@@ -107,18 +107,21 @@ function createDatGui() {
 	f1.add(camera.position, 'z',-50,50).listen();
 }
 
-var leftLeg, rightLeg, Legs;
+var charObject, leftLeg, rightLeg, Legs;
 
-var ui = {factor:100};
+var ui = {factor:100,
+        walkSpeed:1};
 
 function createBlob() {
+    charObject = new THREE.Object3D();
+
     var legGeometry = new THREE.CylinderGeometry(0.5,0.4,3,32);
+    legGeometry.translate(0,-1.5,0);
     var material = new THREE.MeshLambertMaterial({color:0xffdd00});
 
     var leftLegMesh = new THREE.Mesh(legGeometry, material);
     leftLeg = new THREE.Object3D();
-
-    leftLeg.position.y = 1.5;
+    leftLeg.position.y = 3;
     leftLeg.position.z = 0;
 
 
@@ -126,12 +129,12 @@ function createBlob() {
 
     leftLeg.rotation.y = 0.1;
 
-    rightLeg.position.y = 1.5;
+    rightLeg.position.y = 3;
     rightLeg.position.z = 0;
     rightLeg.rotation.y = -0.1;
 
     var foot = new THREE.Mesh(new THREE.BoxGeometry(1,0.7,1.5), material);
-    foot.position.y = -1.15;
+    foot.position.y = -2.7;
     foot.position.z = 0.3;
 
     rightLeg.position.x = -0.8;
@@ -147,22 +150,31 @@ function createBlob() {
     Legs.add(leftLeg);
     Legs.add(rightLeg);
 
-    scene.add(Legs);
+    charObject.add(Legs);
 
-    var f1 = gui.addFolder('Right Leg');
+
+
+    var f1 = gui.addFolder('Right Leg Rotation');
 	f1.add(rightLeg.rotation, 'x',-180*Math.PI/180,180*Math.PI/180).listen();
 	f1.add(rightLeg.rotation, 'y',-180*Math.PI/180,180*Math.PI/180);
 	f1.add(rightLeg.rotation, 'z',-180*Math.PI/180,180*Math.PI/180);
 
-	var f2 = gui.addFolder('Left Leg');
+	var f2 = gui.addFolder('Left Leg Rotation');
 	f2.add(leftLeg.rotation, 'x',-180*Math.PI/180,180*Math.PI/180).listen();
 	f2.add(leftLeg.rotation, 'y',-180*Math.PI/180,180*Math.PI/180);
 	f2.add(leftLeg.rotation, 'z',-180*Math.PI/180,180*Math.PI/180);
 
-    gui.add(ui,'factor',100,1000);
+    gui.add(ui,'factor',50,1000);
+    gui.add(ui,'walkSpeed',1,5);
 
 
-    var torsoGeometry = new THREE.BoxGeometry()
+    var torsoGeometry = new THREE.BoxGeometry(3,3,3);
+    var torsoMesh = new THREE.Mesh(torsoGeometry, material);
+    torsoMesh.position.set(0,4,0);
+
+    charObject.add(torsoMesh);
+
+    scene.add(charObject);
 }
 
 function createLights() {
@@ -197,16 +209,16 @@ function stopWalking() {
 
 function loop(){
     if(wPressed) {
-        Legs.translateZ(0.1);
+        charObject.translateZ(ui.walkSpeed*0.1);
     }
     if(sPressed) {
-        Legs.translateZ(-0.1);
+        charObject.translateZ(ui.walkSpeed*-0.1);
     }
     if(aPressed) {
-        Legs.rotateY(3*Math.PI/180);
+        charObject.rotateY(3*Math.PI/180);
     }
     if(dPressed) {
-        Legs.rotateY(-3*Math.PI/180);
+        charObject.rotateY(-3*Math.PI/180);
     }
 
     if(wPressed || sPressed || aPressed || dPressed) {
