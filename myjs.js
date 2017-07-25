35d22bf133ffd8f06a7d0092388b145e4b265bb3
Eshan Shah <eshanshah7@gmail.com>
2017-07-25 12:46:54 -0700

    First commit
    Control walking in 3d with keyboard

---
+++ myjs.js
@@ -0,0 +1,225 @@
+// Scene variables
+var scene, camera, fieldOfView, aspectRatio, nearPlane, farPlane, HEIGHT, WIDTH, renderer, container, controls;
+
+// Light variables
+var directionalLight, pointLight, hemisphereLight, ambientLight;
+
+//dat-gui
+var gui = new dat.GUI();
+
+window.addEventListener('load', init, false);
+
+function init(event) {
+    createScene();
+    createHelpers();
+    createBlob();
+    createLights();
+
+    createDatGui();
+    // document.addEventListener('mousemove', onMouseMove, false);
+    loop();
+}
+
+function createScene() {
+    HEIGHT = window.innerHeight;
+    WIDTH = window.innerWidth;
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xeeeeee);
+
+    aspectRatio = WIDTH/HEIGHT;
+    fieldOfView = 45;
+    nearPlane = 1;
+    farPlane = 10000;
+
+    camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane);
+    camera.position.set(10,5,10);
+
+    renderer = new THREE.WebGLRenderer({alpha: true, antialias: true});
+    renderer.setSize(WIDTH,HEIGHT);
+    renderer.shadowMap.enabled = true;
+
+    container = document.querySelector('#container');
+    container.appendChild(renderer.domElement);
+
+    controls = new THREE.OrbitControls(camera);
+
+    document.addEventListener('keydown', keyDownHandler, false);
+    document.addEventListener('keyup', keyUpHandler, false);
+
+    window.addEventListener('resize', onWindowResize, false);
+}
+
+var wPressed, aPressed, sPressed, dPressed;
+
+var walking;
+function keyDownHandler(e) {
+    // console.log(e.keyCode);
+    if(e.keyCode == 87) {
+        wPressed = true;
+    }
+    if(e.keyCode == 65) {
+        aPressed = true;
+    }
+    if(e.keyCode == 83) {
+        sPressed = true;
+    }
+    if(e.keyCode == 68) {
+        dPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    // console.log(e.keyCode);
+    if(e.keyCode == 87) {
+        wPressed = false;
+        walking = false;
+    }
+    if(e.keyCode == 65) {
+        aPressed = false;
+        walking = false;
+    }
+    if(e.keyCode == 83) {
+        sPressed = false;
+        walking = false;
+    }
+    if(e.keyCode == 68) {
+        dPressed = false;
+        walking = false;
+    }
+}
+
+function createHelpers() {
+    var size = 100;
+    var divisions = 100;
+
+    var gridHelper = new THREE.GridHelper( size, divisions );
+    scene.add( gridHelper );
+
+    var axisHelper = new THREE.AxisHelper( size/2 );
+    scene.add( axisHelper );
+}
+
+function createDatGui() {
+    var f1 = gui.addFolder('Position');
+	f1.add(camera.position, 'x',-50,50).listen();
+	f1.add(camera.position, 'y',-50,50).listen();
+	f1.add(camera.position, 'z',-50,50).listen();
+}
+
+var leftLeg, rightLeg, Legs;
+
+var ui = {factor:100};
+
+function createBlob() {
+    var legGeometry = new THREE.CylinderGeometry(0.5,0.4,3,32);
+    var material = new THREE.MeshLambertMaterial({color:0xffdd00});
+
+    var leftLegMesh = new THREE.Mesh(legGeometry, material);
+    leftLeg = new THREE.Object3D();
+
+    leftLeg.position.y = 1.5;
+    leftLeg.position.z = 0;
+
+
+    rightLeg = leftLeg.clone();
+
+    leftLeg.rotation.y = 0.1;
+
+    rightLeg.position.y = 1.5;
+    rightLeg.position.z = 0;
+    rightLeg.rotation.y = -0.1;
+
+    var foot = new THREE.Mesh(new THREE.BoxGeometry(1,0.7,1.5), material);
+    foot.position.y = -1.15;
+    foot.position.z = 0.3;
+
+    rightLeg.position.x = -0.8;
+    rightLeg.add(foot.clone());
+    rightLeg.add(leftLegMesh.clone());
+
+    leftLeg.position.x = 0.8;
+    leftLeg.add(foot);
+    leftLeg.add(leftLegMesh);
+
+    Legs = new THREE.Object3D();
+
+    Legs.add(leftLeg);
+    Legs.add(rightLeg);
+
+    scene.add(Legs);
+
+    var f1 = gui.addFolder('Right Leg');
+	f1.add(rightLeg.rotation, 'x',-180*Math.PI/180,180*Math.PI/180).listen();
+	f1.add(rightLeg.rotation, 'y',-180*Math.PI/180,180*Math.PI/180);
+	f1.add(rightLeg.rotation, 'z',-180*Math.PI/180,180*Math.PI/180);
+
+	var f2 = gui.addFolder('Left Leg');
+	f2.add(leftLeg.rotation, 'x',-180*Math.PI/180,180*Math.PI/180).listen();
+	f2.add(leftLeg.rotation, 'y',-180*Math.PI/180,180*Math.PI/180);
+	f2.add(leftLeg.rotation, 'z',-180*Math.PI/180,180*Math.PI/180);
+
+    gui.add(ui,'factor',100,1000);
+
+
+    var torsoGeometry = new THREE.BoxGeometry()
+}
+
+function createLights() {
+    directionalLight = new THREE.DirectionalLight(0xffffff, 0.9);
+    directionalLight.position.set(150,350,350);
+    directionalLight.castShadow = true;
+
+    ambientLight = new THREE.AmbientLight(0xdc8874, .5);
+
+    scene.add(directionalLight);
+    scene.add(ambientLight);
+}
+
+function onWindowResize() {
+	HEIGHT = window.innerHeight;
+    WIDTH = window.innerWidth;
+	camera.aspect = window.innerWidth/window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function startWalking() {
+    rightLeg.rotation.x = Math.cos((Date.now()/ui.factor) + 90*Math.PI/180);
+    leftLeg.rotation.x = Math.sin(Date.now()/ui.factor);
+}
+
+function stopWalking() {
+    rightLeg.rotation.x = 0;
+    leftLeg.rotation.x = 0;
+}
+
+function loop(){
+    if(wPressed) {
+        Legs.translateZ(0.1);
+    }
+    if(sPressed) {
+        Legs.translateZ(-0.1);
+    }
+    if(aPressed) {
+        Legs.rotateY(3*Math.PI/180);
+    }
+    if(dPressed) {
+        Legs.rotateY(-3*Math.PI/180);
+    }
+
+    if(wPressed || sPressed || aPressed || dPressed) {
+        walking = true;
+    }
+
+    if(walking) {
+        startWalking();
+    } else {
+        stopWalking();
+    }
+    controls.update();
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(loop);
+}
